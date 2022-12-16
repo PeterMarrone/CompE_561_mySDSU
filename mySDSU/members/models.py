582ab43f9209aa73553ff2ea0727d0944d069e80
Peter Marrone <marronepeter@yahoo.com>
2022-12-15 22:47:55 -0800

    Quick change to homepage

--- mySDSU/members/models.py
+++ mySDSU/members/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.conf import settings
 
 class Classes(models.Model):
     courseNumber = models.CharField('Course Number', max_length = 100)
--- mySDSU/members/views.py
+++ mySDSU/members/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
+from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.db.models import Q
-from django.views.generic import TemplateView
 from .forms import appointmentForm
 from .forms import applyGradForm
 from .models import Classes
